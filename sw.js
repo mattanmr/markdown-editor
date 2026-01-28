77600fb9b539ef076edc341921cae390eb1cb501
Mattan Ram <mattanmr@gmail.com>
2026-01-28 09:38:39 +0200

    Add PWA support with service worker and offline functionality

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'markdown-editor-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './styles.css',
+    './script.js',
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/marked@11.1.1/marked.min.js'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                // If both cache and network fail, show offline page
+                return caches.match('./index.html');
+            })
+    );
+});
+
+// Update service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
